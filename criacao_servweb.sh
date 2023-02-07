d0d1eaf7271c0fa2634eb4bd54a72fef76f6110e
Marcos Paulo Oliveira Fabbri <88397681+mpfabbri@users.noreply.github.com>
2023-02-07 09:42:21 -0300

    Update criacao_servweb.sh

--- criacao_servweb.sh
+++ criacao_servweb.sh
@@ -15,10 +15,10 @@ apt-get install unzip -y
 echo "Etapa 3 - Baixando Arquivos do Servidor"
 
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip -P /tmp/
-unzip /tmp/main.zip
+unzip /tmp/main.zip -d /tmp/
 
 echo "Etapa 4 - Copiando Arquivos do Servidor"
 
-cp -R /tmp/linux-site-dio-main/ /var/www/html/
+cp -R /tmp/linux-site-dio-main/* /var/www/html/
 
 echo "Processo Finalizado!"
